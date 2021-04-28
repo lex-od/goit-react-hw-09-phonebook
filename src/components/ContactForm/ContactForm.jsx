c03009eea3152493a7c5afcdbe1943c1c30e5233
Aleksey Yesin <esinru@gmail.com>
2021-04-28 16:47:50 +0300

    Adds ContactForm refat to hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,92 +1,77 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import css from './ContactForm.module.scss';
 import config from '../../json/ContactFormConfig.json';
 import { contactsSls, contactsOps } from '../../redux/contacts/';
 
-class ContactForm extends Component {
-    static propTypes = {
-        dispSubmit: PropTypes.func.isRequired,
-        contacts: PropTypes.arrayOf(
-            PropTypes.shape({
-                id: PropTypes.string.isRequired,
-                name: PropTypes.string.isRequired,
-                number: PropTypes.string.isRequired,
-            }),
-        ).isRequired,
-    };
+export default function ContactForm() {
+    const dispatch = useDispatch();
 
-    state = {
-        name: '',
-        number: '',
-    };
+    const contacts = useSelector(contactsSls.getAllContacts);
 
-    handleChange = e => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+
+    const handleChange = e => {
         const { name, value } = e.target;
-        this.setState({ [name]: value });
+
+        switch (name) {
+            case 'name':
+                setName(value);
+                break;
+            case 'number':
+                setNumber(value);
+                break;
+            default:
+                console.warn(`Тип поля "${name}" не обрабатывается!`);
+        }
     };
 
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
 
-        if (this.isContactExists()) {
-            alert(`Контакт "${this.state.name}" уже существует`);
+        if (isContactExists()) {
+            alert(`Контакт "${name}" уже существует`);
             return;
         }
 
-        this.props.dispSubmit(this.state);
+        dispatch(contactsOps.addContact({ name, number }));
 
-        this.setState({ name: '', number: '' });
+        setName('');
+        setNumber('');
+
+        function isContactExists() {
+            const normName = name.toLowerCase();
+
+            return contacts.some(({ name }) => name.toLowerCase() === normName);
+        }
     };
 
-    isContactExists() {
-        const normName = this.state.name.toLowerCase();
-
-        return this.props.contacts.some(
-            ({ name }) => name.toLowerCase() === normName,
-        );
-    }
-
-    render() {
-        const { name, number } = this.state;
-
-        return (
-            <form onSubmit={this.handleSubmit} autoComplete="off">
-                <input
-                    type="text"
-                    name="name"
-                    pattern={config.INPUT_NAME_PATTERN}
-                    title={config.INPUT_NAME_TITLE}
-                    value={name}
-                    onChange={this.handleChange}
-                    required
-                    className={css.input}
-                    placeholder="Имя..."
-                />
-                <input
-                    type="tel"
-                    name="number"
-                    pattern={config.INPUT_TEL_PATTERN}
-                    title={config.INPUT_TEL_TITLE}
-                    value={number}
-                    onChange={this.handleChange}
-                    required
-                    className={css.input}
-                    placeholder="Телефон..."
-                />
-                <button className={css.addContact}>Добавить контакт</button>
-            </form>
-        );
-    }
+    return (
+        <form onSubmit={handleSubmit} autoComplete="off">
+            <input
+                type="text"
+                name="name"
+                pattern={config.INPUT_NAME_PATTERN}
+                title={config.INPUT_NAME_TITLE}
+                value={name}
+                onChange={handleChange}
+                required
+                className={css.input}
+                placeholder="Имя..."
+            />
+            <input
+                type="tel"
+                name="number"
+                pattern={config.INPUT_TEL_PATTERN}
+                title={config.INPUT_TEL_TITLE}
+                value={number}
+                onChange={handleChange}
+                required
+                className={css.input}
+                placeholder="Телефон..."
+            />
+            <button className={css.addContact}>Добавить контакт</button>
+        </form>
+    );
 }
-
-const mapStateToProps = state => ({
-    contacts: contactsSls.getAllContacts(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-    dispSubmit: contact => dispatch(contactsOps.addContact(contact)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
